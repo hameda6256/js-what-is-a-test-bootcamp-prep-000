4dcab5323e6cf55f68af92115aa0112367de5204
Adham  Hamed <hameda6256@gmail.com>
2019-08-13 06:33:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+if (name === "Susan"){
+  console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Susan")
+}
+
+if (height === 70 ){
+  console.log("The height is correct")
+} else {
+  console.log("Expected the heig
